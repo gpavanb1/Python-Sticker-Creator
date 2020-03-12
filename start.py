90f13966da2ce36fce898b58b5909061646c8fa3
Pavan BG <gpbabc@yahoo.com>
2020-03-12 10:08:15 +0530

    mod start

--- start.py
+++ start.py
@@ -5,5 +5,9 @@ import dash_html_components as html
 from app import app
 
 
+server = app.server
+
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(host='0.0.0.0',
+    port=80,
+    debug=False)
